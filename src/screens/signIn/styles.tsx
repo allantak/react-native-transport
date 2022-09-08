52095c06c197b35d2db2a0332905e4887e0d9503
Lucas Vilela <lucas.vilela.oliveira@gmail.com>
2022-09-08 15:17:26 -0300

    New design Login and Register

--- src/screens/signIn/styles.tsx
+++ src/screens/signIn/styles.tsx
@@ -16,24 +16,66 @@ export const Content = styled.div`
 `;
 
 export const ContentLogo = styled.div`
-  height: 40vh;
-  flex-direction: column;
-  text-align: center;
-  margin-top: 40px;
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+  width: 32.5%;
+  height: 13vh ;
+  padding-left: 10px ;
 `;
 
-export const NameLogo = styled.h1`
-    font-size: 20px;
+export const ContentDescription = styled.div`
+  height: 22vh;
+  margin-top: 10px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+
+export const Register = styled.div`
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
+`;
+
+export const NameLogo = styled.h2`
+    font-size: 15px;
     color: ${AppStyles.colour.font};
     padding: 10px;
     
 `;
 
 export const Description = styled.p`
-    font-size: 13px;
+    font-size: 18px;
+    color: ${AppStyles.colour.font};
+    text-align: flex-start;
+    padding: 10px;
+`;
+
+export const TitleDescription = styled.h1`
+    font-size: 35px;
     color: ${AppStyles.colour.font};
     text-align: center;
-    padding: 10px;
+    padding-left: 10px;
+    margin: 0;
+`;
+
+export const TextButton = styled.h6`
+  font-size: 15px;
+  color: ${AppStyles.colour.primary};
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  text-align: center;
+  padding-left: 10px;
+  margin: 0;
+`;
+export const BeforeButton = styled.h6`
+  font-size: 15px;
+  color: ${AppStyles.colour.font};
+  text-align: center;
+  padding-left: 10px;
+  margin: 0;
 `;
 
 export const SpanError = styled.h6`
@@ -43,11 +85,17 @@ export const SpanError = styled.h6`
 `;
 
 export const ContentInput = styled.div`
-  height: 30vh;
+  height: 25vh;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
 `;
 
 export const ContentButton = styled.div`
-  height: 25vh;
+  height: 40vh;
+  flex-direction: column;
+  justify-content: center;
+  display: flex;
 `;
 
 export const styles = StyleSheet.create({
--- src/screens/signUp/styles.tsx
+++ src/screens/signUp/styles.tsx
@@ -30,10 +30,31 @@ export const ContentHeaders = styled.div`
   height: 10vh;
 `;
 export const ContentInput = styled.div`
-  height: 70vh;
+  height: 38vh;
 `;
 export const ContentButton = styled.div`
-  height: 10vh;
+  height: 30vh;
+`;
+export const Description = styled.p`
+    font-size: 18px;
+    color: ${AppStyles.colour.font};
+    text-align: flex-start;
+    padding: 10px;
+`;
+
+export const TitleDescription = styled.h1`
+    font-size: 30px;
+    color: ${AppStyles.colour.font};
+    text-align: center;
+    padding-left: 10px;
+    margin: 0;
+`;
+export const ContentDescription = styled.div`
+  height: 22vh;
+  margin-top: 10px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
 `;
 
 export const styles = StyleSheet.create({
